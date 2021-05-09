3df10fb76bbcb20c072ab2ea434c8c25a43cf523
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-05-09 11:46:44 +0900

    CSSを追加

--- src/components/myform.vue
+++ src/components/myform.vue
@@ -1,16 +1,21 @@
 <template>
-  <div>
-    <div v-for="memo in memos" :key="memo.id">
-      <p @click="show(memo)">{{ memo.title }}</p>
+  <div class="memobody">
+    <div class="memolist">
+      <div v-for="memo in memos" :key="memo.id">
+        <p @click="show(memo)" class="memotitle">{{ memo.title }}</p>
+      </div>
+      <p v-if="memos.length < 1">メモはありません。</p>
+      <p @click="add">+</p>
     </div>
-    <p v-if="memos.length < 1">保存されているメモはありません。</p>
-    <p @click="add">+</p>
-    <textarea v-model="memo" rows="15" cols="40"></textarea>
-    <button @click="addMemo" v-if="status === 'new'">追加</button>
-    <template v-else-if="status === 'edit'">
-      <button @click="update">更新</button>
-      <button @click="remove">削除</button>
-    </template>
+    <div class="form_with_button">
+      <textarea v-model="memo" rows="12" cols="30"></textarea>
+      <br>
+      <button @click="addMemo" v-if="status === 'new'">追加</button>
+      <template v-else-if="status === 'edit'">
+        <button @click="update">更新</button>
+        <button @click="remove">削除</button>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -26,14 +31,11 @@ export default {
       id: 1,
       status: false,
       editId: null,
-      ary1: []
     }
   },
   mounted() {
     if (localStorage.getItem('memos')) {
       this.memos = JSON.parse(localStorage.getItem('memos'));
-      let ids = this.memos.map(memo => memo.id)
-      this.id = ids.reduce((a, b) => Math.max(a, b)) + 1;
     }
   },
   methods: {
@@ -42,6 +44,10 @@ export default {
         return;
       }
       let title = this.memo.split('\n')[0]
+      if (this.memos[0]) {
+        let ids = this.memos.map(memo => memo.id)
+        this.id = ids.reduce((a, b) => Math.max(a, b)) + 1
+      }
       this.memos.push({id: this.id, title: title, body: this.memo});
       this.memo = '';
       this.id++
@@ -67,7 +73,7 @@ export default {
       this.saveMemos();
     },
     remove() {
-      let index = this.memos.map(memo => memo.id).indexOf(this.editId);
+      let index = this.memos.findIndex(memo => memo.id === this.editId);
       this.memos.splice(index, 1);
       this.saveMemos();
       this.memo = '';
@@ -78,5 +84,28 @@ export default {
 </script>
 
 <style scoped>
-
+template {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.memobody {
+  display: flex;
+  justify-content: center;
+}
+.memolist {
+  width: 200px;
+}
+.memolist p {
+  font-size: 20px;
+}
+.form_with_button {
+  width: 400px;
+}
+textarea {
+  font-size: 18px;
+}
 </style>
